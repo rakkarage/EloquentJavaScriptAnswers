ce3fd57d63e576dd41c0fec76c9a954108e6aff2
rakkarage <rakkarage@gmail.com>
2018-02-10 12:40:29 -0500

    x

---
+++ chapter2/1.js
@@ -0,0 +1,5 @@
+var data = '#';
+for (var x = 0; x < 7; x++) {
+	console.log(data);
+	data += '#';
+}
---
+++ chapter2/2.js
@@ -0,0 +1,13 @@
+for (var x = 1; x <= 100; x++)
+{
+	var five = (x % 5) == 0;
+	var three = (x % 3) == 0;
+	if (three && five)
+		console.log('FizzBuzz');
+	if (three)
+		console.log('Fizz');
+	else if (five)
+		console.log('Buzz');
+	else
+		console.log(x);
+}
