95727819b5db6ee78300f9344c81ecf07337cdca
rakkarage <rakkarage@gmail.com>
2018-02-11 17:37:51 -0500

    x

---
+++ chapter4/2.js
@@ -0,0 +1,18 @@
+function reverseArray(array) {
+	let newArray = [];
+	for (let item of array)
+		newArray.unshift(item);
+	return newArray;
+}
+function reverseArrayInPlace(array) {
+	let half = Math.floor(array.length / 2);
+	for (let i = 0; i < half; i++) {
+		let swap = array[i];
+		array[i] = array[array.length - 1 - i];
+		array[array.length - 1 - i] = swap;
+	}
+}
+console.log(reverseArray(['A', 'B', 'C']));
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
