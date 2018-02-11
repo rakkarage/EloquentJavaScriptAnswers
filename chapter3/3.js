8dafcb555c6b5807ac0afa4d7f4a1acc5c6d7c6f
rakkarage <rakkarage@gmail.com>
2018-02-10 22:50:07 -0500

    x

---
+++ chapter3/3.js
@@ -0,0 +1,13 @@
+function countChar(value, character) {
+	let count = 0;
+	for (let i = 0; i < value.length; i++) {
+		if (value[i] == character)
+			count++;
+	}
+	return count;
+}
+function countB(value) {
+	return countChar(value, 'B');
+}
+console.log(countB('BBC'));
+console.log(countChar('kakkerlak', 'k'));
