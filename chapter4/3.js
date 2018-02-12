b80713de08212378b936de48488154857bd00bcc
rakkarage <rakkarage@gmail.com>
2018-02-11 22:16:59 -0500

    x

--- chapter4/3.js
+++ chapter4/3.js
@@ -26,7 +26,7 @@ function nth(list, n) {
 	return (node == null) ? undefined : node.value;
 }
 function nthRecursive(list, n) {
-	if (n == 0) return list.value;
+	if (n == 0 || list.rest == null) return list.value;
 	else return nthRecursive(list.rest, n - 1);
 }
 logList(arrayToList([10, 20, 30, 40, 50]));
@@ -35,4 +35,5 @@ console.log(listToArray(arrayToList([10, 20, 30])));
 logList(prepend(10, prepend(20, null)));
 console.log(nth(arrayToList([10, 20, 30]), 1));
 console.log(nth(arrayToList([10, 20, 30]), 5));
-console.log(nthRecursive(arrayToList([11, 22, 33, 44, 55, 66, 77]), 4));
+console.log(nthRecursive(arrayToList([0, 11, 22, 33, 44, 55, 66, 77]), 3));
+console.log(nthRecursive(arrayToList([0, 11, 22, 33, 44, 55, 66, 77]), 9));
