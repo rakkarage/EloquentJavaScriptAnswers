8cbc05a2a64f4f1194c281a7fa4ec017b0f0bdef
Rakka Rage <rakkarage@gmail.com>
2018-02-14 14:46:34 -0500

    x

--- chapter5/3.js
+++ chapter5/3.js
@@ -5,11 +5,11 @@ function every(array, test) {
 	return true;
 }
 function everySome(array, test) {
-	return !array.some(test);
+	return !array.some(i => !test(i));
 }
 console.log(every([1, 3, 5], n => n < 10));
 console.log(every([2, 4, 16], n => n < 10));
 console.log(every([], n => n < 10));
-console.log(everySome([1, 3, 5], n => !(n < 10)));
-console.log(everySome([2, 4, 16], n => !(n < 10)));
+console.log(everySome([1, 3, 5], n => n < 10));
+console.log(everySome([2, 4, 16], n => n < 10));
 console.log(everySome([], n => !(n < 10)));
