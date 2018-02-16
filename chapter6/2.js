a80be626a8fcf07f6d44d8e9aa64fad6755131b7
Rakka Rage <rakkarage@gmail.com>
2018-02-16 18:46:08 -0500

    Group

---
+++ chapter6/2.js
@@ -0,0 +1,28 @@
+class Group {
+	constructor() {
+		this.array = []
+	}
+	static from(o) {
+		let g = new Group()
+		for (let i = 0; i < o.length; i++)
+			g.add(o[i])
+		return g;
+	}
+	add(i) {
+		if (!this.has(i))
+			this.array.unshift(i)
+	}
+	delete(i) {
+		if (this.has(i))
+			this.array = this.array.filter(t => t !== i)
+	}
+	has(i) {
+		return this.array.includes(i)
+	}
+}
+let group = Group.from([10, 20])
+console.log(group.has(10))
+console.log(group.has(30))
+group.add(10)
+group.delete(10)
+console.log(group.has(10))
