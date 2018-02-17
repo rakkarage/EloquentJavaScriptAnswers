570c9871bb8ab7442a8f2e8d963bf530436f506e
Rakka Rage <rakkarage@gmail.com>
2018-02-17 18:14:31 -0500

    X

--- chapter7/1.js
+++ chapter7/1.js
@@ -6,45 +6,45 @@ const roads = [
 	"Grete's House-Shop", 'Marketplace-Farm',
 	'Marketplace-Post Office', 'Marketplace-Shop',
 	'Marketplace-Town Hall', 'Shop-Town Hall'
-];
+]
 function buildGraph(edges) {
-	let graph = Object.create(null);
+	let graph = Object.create(null)
 	function addEdge(from, to) {
 		if (graph[from] == null) {
-			graph[from] = [to];
+			graph[from] = [to]
 		} else {
-			graph[from].push(to);
+			graph[from].push(to)
 		}
 	}
 	for (let [from, to] of edges.map(r => r.split('-'))) {
-		addEdge(from, to);
-		addEdge(to, from);
+		addEdge(from, to)
+		addEdge(to, from)
 	}
-	return graph;
+	return graph
 }
-const roadGraph = buildGraph(roads);
+const roadGraph = buildGraph(roads)
 class VillageState {
 	constructor(place, parcels) {
-		this.place = place;
-		this.parcels = parcels;
+		this.place = place
+		this.parcels = parcels
 	}
 	move(destination) {
 		if (!roadGraph[this.place].includes(destination)) {
-			return this;
+			return this
 		} else {
 			let parcels = this.parcels.map(p => {
-				if (p.place != this.place) return p;
-				return { place: destination, address: p.address };
-			}).filter(p => p.place != p.address);
-			return new VillageState(destination, parcels);
+				if (p.place != this.place) return p
+				return { place: destination, address: p.address }
+			}).filter(p => p.place != p.address)
+			return new VillageState(destination, parcels)
 		}
 	}
 }
 let first = new VillageState(
 	'Post Office',
 	[{ place: 'Post Office', address: "Alice's House" }]
-);
-let next = first.move("Alice's House");
-console.log(next.place);
-console.log(next.parcels);
-console.log(first.place);
+)
+let next = first.move("Alice's House")
+console.log(next.place)
+console.log(next.parcels)
+console.log(first.place)
