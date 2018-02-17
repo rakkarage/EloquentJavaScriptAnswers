11a184552361201a178c4c69ac19d537d828eac6
Rakka Rage <rakkarage@gmail.com>
2018-02-17 09:46:05 -0500

    group2

--- chapter6/3.js
+++ chapter6/3.js
@@ -1,28 +1,16 @@
-class Group2Iterator {
-	constructor(group2) {
-		this.i = 0
-		this.group2 = group2
-	}
-	next() {
-		return { value: this.group2.array[this.i++], done: this.group2.array.includes[this.i] }
-	}
-	get value() {
-		return this.group2.array[this.i]
-	}
-}
 class Group2 {
 	constructor() {
 		this.array = []
 	}
 	static from(o) {
 		let g = new Group2()
-		for (let i = 0; i < o.length; i++)
-			g.add(o[i])
+		for (let i of o)
+			g.add(i)
 		return g
 	}
 	add(i) {
 		if (!this.has(i))
-			this.array.unshift(i)
+			this.array.push(i)
 	}
 	delete(i) {
 		if (this.has(i))
@@ -32,9 +20,23 @@ class Group2 {
 		return this.array.includes(i)
 	}
 	[Symbol.iterator]() {
-		return new Group2Iterator();
+		return new Group2Iterator(this)
 	}
 }
-for (let value of Group2.from(["a", "b", "c"])) {
-	console.log(value);
+class Group2Iterator {
+	constructor(group2) {
+		this.i = 0
+		this.group2 = group2
+	}
+	next() {
+		if (this.i >= this.group2.array.length)
+			return { value: undefined, done: true }
+		return { value: this.group2.array[this.i++], done: false }
+	}
+	value() {
+		return this.group2.array[this.i]
+	}
+}
+for (let value of Group2.from(['a', 'b', 'c'])) {
+	console.log(value)
 }
